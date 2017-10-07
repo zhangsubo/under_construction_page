c9e9f16e7bc223944a30940de28f12ca40b6ad4b
张小璋 <account@zhangsubo.cn>
2017-10-07 10:45:27 +0800

    create

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.quote', {
+    strings: ['The last one is the best one.','Je pense donc je suis.'],
+    typeSpeed: 100,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+});
+
+var myDate = new Date(),
+    yearData = myDate.getFullYear(); 
+$("#year").html(yearData);
