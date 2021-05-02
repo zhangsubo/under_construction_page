bbc70b20947a6fa2b992693e131c6083ae3cce02
张小璋 <account@zhagnsubo.cn>
2021-05-02 20:05:41 +0800

    修改为读取配置前端展示

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ var typed = new Typed('#quote', {
 var myDate = new Date(),
     yearData = myDate.getFullYear(); 
 $("#year").html(yearData);
+
+
+// 通过配置决定前端显示内容
+
+var site_name = '';
+var icp = '';
+
+if (site_name.length !=0) {
+    $('.site_name').html(site_name);
+    $('.site_name').attr("href","//"+site_name);
+    document.title = site_name+'建设中';
+}
+if (icp.length != 0) {
+    $('#icp').attr("href","https://beian.miit.gov.cn/");
+    $('#icp').attr("target","_blank");
+    $('#icp').attr("content","nofollow");
+    $('#icp').html(icp);
+}
+
+
